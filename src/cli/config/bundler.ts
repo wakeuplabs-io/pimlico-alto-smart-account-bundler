c2d1aa05f5902e708a5b3c18feb57b4b46a9cb53
mous <97399882+mouseless-eth@users.noreply.github.com>
2024-04-22 15:16:19 +0300

    small fix

--- src/cli/config/bundler.ts
+++ src/cli/config/bundler.ts
@@ -21,9 +21,10 @@ export const bundlerArgsSchema = z.object({
             )
             return validatedAddresses
         }),
-    "entrypoint-simulation-contract": z.preprocess((val) => {
-        val === "" ? undefined : val
-    }, addressSchema.optional()),
+    "entrypoint-simulation-contract": z.preprocess(
+        (v) => (v === "" ? undefined : v),
+        addressSchema.optional()
+    ),
     "safe-mode": z.boolean(),
     "utility-private-key": hexData32Schema
         .transform((val) => privateKeyToAccount(val) satisfies Account)
